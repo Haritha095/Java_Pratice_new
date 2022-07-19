87f55e7a71e9e6f07caf00f4b65c7f56acb24246
Haritha095 <101257478+Haritha095@users.noreply.github.com>
2022-07-19 11:46:32 +0530

    Update Gifts.java

--- src/Gifts.java
+++ src/Gifts.java
@@ -1,26 +1,33 @@
+/*
+* Gifts class to show the exceptional handling concept
+ Created two arrays one for student and sweet both of equal length and  tried dividing
+ each sweetbox with section of student and 
+ in one case as section has zero student we get Arithmetic Exception which is an
+ finally()-block to be executed at any cost if the function exists
+* */
+  
 public class Gifts extends Exception
 {
+
 public static void main(String args[])
 {
-    /*Created wtwo arrays one for student and sweet both of equal length and  when divided each sweetbox
-     with section of student and in one case as section has zero student we get Arithmetic Exception which is an
-      example of unchecked exception */
+   
   int student[] = {67, 78, 67, 89, 0 };
   int sweet[] = {34, 78, 121, 78, 44};
-  int[] numsweet = new int[5];
+  int[] numberofsweet = new int[5];
   try /*this block is to include the code which we feel can get exceptions*/
   {
     for (int i = 0; i < 5; i++)
     {
-     numsweet[i] = sweet[i] / student[i];
+        numberofsweet[i] = sweet[i] / student[i];
      }
      }
-     catch (ArithmeticException error) /*executed when exception occurs*/
+     catch (ArithmeticException error) 
      {
      System.out.println("Arithmetic Exception");
      System.out.println(error.getMessage() + "error .");
      }
-     finally /*set of code to be executed at any cost*/
+     finally 
      {
      System.out.println("error");
       }
